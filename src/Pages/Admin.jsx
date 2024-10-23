7aa282ec4435bd20266a868fdc49cd1871dccb8f
Akash Biswas <akashrahul2006@gmail.com>
2024-10-23 22:18:33 +0530

    admin updated

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -6,7 +6,7 @@ import UserStatus from "../UI/UserStatus";
 const Admin = () => {
   return (
     <div className="bg-[#202c37] h-screen w-screen text-white overflow-x-hidden">
-      <div className="py-8 md:py-12">
+      <div className="py-8 md:py-4">
         <UserHeading text="Akash" />
       </div>
       <AdminFromData />
